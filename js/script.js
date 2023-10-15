aba1f9321e6fd70ba6c4beafc43e8eb18e2d7212
ridhofaauzi <ridhofauzi543@gmail.com>
2023-10-15 22:31:04 +0700

    update terbaru

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-const imgCourse1 = document.querySelector('.image-course1');
-const imgCourse2 = document.querySelector('.image-course2');
+const imgCourse1 = document.querySelector(".image-course1");
+const imgCourse2 = document.querySelector(".image-course2");
 
 function lebarlayar() {
   let lebarLayar = window.innerWidth;
 
   if (lebarLayar <= 992) {
-    imgCourse1.classList.add('desktop2');
-    imgCourse2.classList.add('desktop1');
-    imgCourse1.classList.remove('desktop1');
-    imgCourse2.classList.remove('desktop2');
+    imgCourse1.classList.add("desktop2");
+    imgCourse2.classList.add("desktop1");
+    imgCourse1.classList.remove("desktop1");
+    imgCourse2.classList.remove("desktop2");
   } else {
-    imgCourse1.classList.add('desktop1');
-    imgCourse2.classList.add('desktop2');
-    imgCourse1.classList.remove('desktop2');
-    imgCourse2.classList.remove('desktop1');
+    imgCourse1.classList.add("desktop1");
+    imgCourse2.classList.add("desktop2");
+    imgCourse1.classList.remove("desktop2");
+    imgCourse2.classList.remove("desktop1");
   }
 }
 
 window.onload = lebarlayar;
 window.onresize = lebarlayar;
+
+// notification-box
+
+let box = document.getElementById("box");
+let down = false;
+
+function toggleNotification() {
+  if (down) {
+    box.style.height = "0px";
+    box.style.opacity = 0;
+    down = false;
+  } else {
+    box.style.height = "auto";
+    box.style.opacity = 1;
+    down = true;
+  }
+}
